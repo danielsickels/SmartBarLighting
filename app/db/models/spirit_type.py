492b295b4f70166cd49b0686731e9dd6b87d52e9
danielsickels <144171988+danielsickels@users.noreply.github.com>
2024-12-20 09:13:07 -0500

    Update spirit_type.py
    Name to be FALSE

--- app/db/models/spirit_type.py
+++ app/db/models/spirit_type.py
@@ -7,7 +7,7 @@ class SpiritType(Base):
     __tablename__ = "spirit_types"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False, unique=False)
 
     # Relationships
     recipes = relationship(
